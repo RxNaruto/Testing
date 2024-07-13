ddd0742d0438610245c2296d686ed884dad807e3
Rithk <rithkchaudhary86@gmail.com>
2024-07-13 09:37:39 +0530

    mocking with prisma

--- vitest-prisma/src/tests/index.test.ts
+++ vitest-prisma/src/tests/index.test.ts
@@ -1,7 +1,12 @@
-import { describe,expect,it } from "vitest";
+import { describe,expect,it,vi } from "vitest";
 import request  from "supertest";
 import { app } from "../index";
+import { prismaClient } from "../db";
 
+
+vi.mock("../db",()=>({
+    prismaClient: {sum: {create: vi.fn()}}
+}))
 describe("POST /sum",()=>{
     it("should return the sum of numbers",async()=>{
         const res = await request(app).post("/sum").send({
@@ -9,7 +14,7 @@ describe("POST /sum",()=>{
             b: 2
         });
         expect(res.statusCode).toBe(200);
-        expect(res.body.ans).toBe(3);
+        expect(res.body.result).toBe(3);
     })
     it("should return 411 if no inputs are provided", async () => {
         const res = await request(app).post("/sum").send({});
